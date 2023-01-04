105f7d3a15349091bc9908bd60791d95b6ccbf91
Mattia Cardillo <mcardillo98@gmail.com>
2023-01-04 17:07:26 +0100

    new_prometheus_target

--- etldatapipeline/configs/prometheus.py
+++ etldatapipeline/configs/prometheus.py
@@ -1,1 +1,2 @@
 prometheushostname="http://15.160.61.227:29090"
+label_config = {'job': 'host'}
--- etldatapipeline/main.py
+++ etldatapipeline/main.py
@@ -3,15 +3,13 @@ from prometheus_api_client import PrometheusConnect, MetricsList, MetricSnapshot
 from datetime import timedelta
 from confluent_kafka import Producer, Consumer
 from prometheus_api_client.utils import parse_datetime
-from configs import prometheus
+from configs import prometheus as prom_config
 
 kafka_config = {'bootstrap.servers': 'kafka:9092'}
 kafka_topic = 'prometheusdata'
 
 app = Flask(__name__)
-prom = PrometheusConnect(url=prometheus.prometheushostname, disable_ssl=True)
-
-label_config = {'job': 'prometheus'}
+prom = PrometheusConnect(url = prom_config.prometheushostname, disable_ssl=True)
 
 def delivery_callback(err, msg):
         if err:
@@ -25,8 +23,8 @@ def hello():
 
 @app.route('/metrics')
 def getMetrics():
-    allMetrics = prom.all_metrics(label_config)
-    return allMetrics
+    allJobMetrics = prom.get_current_metric_value(metric_name= '', label_config = prom_config.label_config)
+    return allJobMetrics
 
 @app.route('/metrics/send')
 def sendKafkaMetrics():
@@ -42,20 +40,30 @@ def sendKafkaMetrics():
 
 @app.route('/metrics/get')
 def getKafkaMetrics():
+    print('Consumer Start')
     c = Consumer({
     'bootstrap.servers': 'kafka:9092',
     'group.id': 'prometheusgroup',
     'auto.offset.reset': 'earliest'
     })
 
-    c.subscribe([kafka_topic])
-    print(c)
-    msg = c.poll(1.0)
-    print(msg)
-    c.close()
+    messageReceived = False;
+
+    while (messageReceived == False):
+        msg = c.poll(1.0)
+        
+        if msg is None:
+            continue
+        if msg.error():
+            print("Errore: {}".format(msg.error()))
+            continue
+
+        # Stampa il messaggio
+        print("Messaggio ricevuto: {}".format(msg.value().decode('utf-8')))
+        messageReceived = True
 
     return 'Received'
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", debug=True)
+    app.run(host="0.0.0.0")
